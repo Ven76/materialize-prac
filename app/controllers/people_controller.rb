a0b9320258f586e2020ec7c105475bac95fdcc40
Tyler Davis <Ryan.Tyler.Davis@gmail.com>
2017-05-26 19:26:06 -0600

    da bears

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,11 +1,14 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
   end
 
   def show
+      @person = Person.find(params[:id})])
   end
 
   def new
+    @people = Person.new
   end
 
   def edit
